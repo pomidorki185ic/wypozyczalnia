252076c0dbbca2d05adbcf482777398f73d18cdf
DawidP <dawidpawlowski98@wp.pl>
2021-01-28 16:26:59 +0100

    BazaDanych_platnosc #POM-142 #POM-148

--- wypozyczenie/models.py
+++ wypozyczenie/models.py
@@ -13,9 +13,15 @@ class Wypozyczenie(models.Model):
      status = models.CharField(max_length=20, default="brak")
      sprzet = models.ForeignKey(Asortyment, on_delete=models.CASCADE)
      klient = models.ForeignKey(Klient, on_delete=models.CASCADE)
+     id_klient = models.CharField(max_length=20,default=1)
+     id_sprzet = models.CharField(max_length=20,default=1)
+     platnosc = models.CharField(max_length=6,default=0)
+     rodzaj_platnosci = models.CharField(max_length=30,default='brak')
+     status_platnosci = models.CharField(max_length=30,default='Do zaplaty')
+     data_platnosci = models.DateField(default='1970-01-01')
   
 
 class WypozyczenieForm(ModelForm):
     class Meta:
         model = Wypozyczenie
-        fields = ['data_rozpoczecia', 'data_zakonczenia', 'godzina_rozpoczecia','godzina_zakonczenia','status','sprzet','klient']
+        fields = ['data_rozpoczecia', 'data_zakonczenia', 'godzina_rozpoczecia','godzina_zakonczenia','status','sprzet','klient','id_klient','id_sprzet','platnosc','rodzaj_platnosci','status_platnosci','data_platnosci']
