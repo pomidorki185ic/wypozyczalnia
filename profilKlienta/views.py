fa6463961e10108eefd94f95638ae3f4e8965daf
DawidP <dawidpawlowski98@wp.pl>
2021-01-28 16:25:48 +0100

    Backend_platnosc #POM-141 #POM-147

--- profilKlienta/views.py
+++ profilKlienta/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView
 from .forms import KlientForm, SzukajKlientaForm
 from .models import Klient
+from sprzet.models import Asortyment
+from wypozyczenie.models import Wypozyczenie
+from wypozyczenie.forms import WypozyczenieForm 
 import sets
+from datetime import date, datetime, timedelta
+
 
 def rejestracja(request):
     
@@ -19,9 +24,50 @@ def rejestracja(request):
     return render(request, 'rejestracja.html', {'form': form})
 
 def base(request):
-    return render(request, 'base.html', {})
-def roboczy(request):
-    return render(request, 'roboczy.html', {})
+    today = date.today()
+    jutro = date.today() + timedelta(days=1)
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    klient = Klient.objects.all()
+    
+    rezerwacja = Wypozyczenie.objects.all()
+    powrot = Wypozyczenie.objects.all()
+    spoznienia = Wypozyczenie.objects.all()
+    d1 = today.strftime("%Y-%m-%d")
+    if d1 and current_time:
+        powrot = Wypozyczenie.objects.filter(Q(data_zakonczenia__icontains=d1) & Q(godzina_zakonczenia__gte=current_time) & Q(status__icontains="Pickup") | Q(data_zakonczenia__icontains=jutro) & Q(status__icontains="Pickup"))
+
+    if d1 and current_time:
+        rezerwacja = Wypozyczenie.objects.filter(Q(data_rozpoczecia__icontains=d1) & Q(godzina_rozpoczecia__gte=current_time) & Q(status__icontains="Rezerwacja") | Q(data_rozpoczecia__icontains=jutro) & Q(status__icontains="Rezerwacja") | Q(data_rozpoczecia__icontains=d1) & Q(godzina_rozpoczecia__lte=current_time) & Q(status__icontains="Rezerwacja"))
+
+    if d1 and current_time:
+        spoznienia = Wypozyczenie.objects.filter(Q(data_zakonczenia__lte=d1) & Q(godzina_zakonczenia__lte=current_time) & Q(status__icontains="Pickup"))
+   
+
+    return render(request, 'Dashboard.html', {"powrot":powrot,"rezerwacja":rezerwacja,"spoznienia":spoznienia,"klient":klient})
+
+
+
+
+def dane_wypozyczenia(request, pk):
+    
+    klient = Klient.objects.all()
+    
+    wypozyczenie = get_object_or_404(Wypozyczenie, pk=pk)
+    if request.method == 'POST':
+        form = WypozyczenieForm(request.POST, instance=wypozyczenie)
+        if form.is_valid():
+            wypozyczenie = form.save(commit=True)
+            wypozyczenie.save()
+            return redirect('base')
+    else:
+        form = WypozyczenieForm(instance=wypozyczenie)
+    query3 = request.GET.get('q3')
+    if query3:
+       klient = Klient.objects.filter(Q(pk__icontains=query3))
+    
+    return render(request, 'dane_wypozyczenia.html', {'form':form,"wypozyczenie":wypozyczenie,'klient':klient})
+
 
 
 def spis_klientow(request):
@@ -43,3 +89,12 @@ def dane_klienta(request, pk):
         form = KlientForm(instance=klient)
     return render(request, "dane_klienta.html",{'form': form,"klient":klient})
 
+def platnosci(request):
+    obj_platnosc = Wypozyczenie.objects.all()
+    do_zaplaty = Wypozyczenie.objects.filter(Q(status_platnosci="Do zaplaty"))
+    zaplacone = Wypozyczenie.objects.filter(Q(status_platnosci="Zapłacony") | Q(status_platnosci="Nadpłata"))
+    return render(request, "platnosci.html",{'do_zaplaty':do_zaplaty,'zaplacone':zaplacone})
+
+def historia(request):
+    historia = Wypozyczenie.objects.all()
+    return render(request, "historia.html",{'historia':historia})
